f9596d5e8dfd69948ae8ff44eee739624313ca95
Anamika <Anamika.Katyal@tatacommunications.com>
2020-04-10 17:17:29 +0530

    subscribe to the store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store= createStore(movies);
+
+console.log("state", store.getState());
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 
 
